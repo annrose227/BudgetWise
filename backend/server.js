7d1fa417f8f01fa3d383bb4e7360834a71451147
Ann <annrosemk227@gmail.com>
2025-06-06 21:06:02 +0530

    backend_render

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,13 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  .connect("mongodb://localhost:27017/budget-balance", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/budget-balance",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
